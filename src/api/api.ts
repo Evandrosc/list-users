a6411f8e5180e946dac0d30d8288cdf34edec8c2
Evandrosc <evandromdsc@gmail.com>
2023-09-09 22:48:41 -0300

    refactor: api

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,18 +3,46 @@ import { User } from "../Tipagem/User";
 
 const BASE_URL = "https://randomuser.me/api/";
 
-async function getUsers(results: number = 1)  {
-    const response = await axios.get(`${BASE_URL}`, {
-        params: {
-            results
+export let usersCache: User[] = []; // Array para armazenar os resultados de getUsers
+
+/**
+ * Obtém um ou mais usuários da API randomuser.me e armazena em cache.
+ * @param results - O número de resultados a serem obtidos da API.
+ * @returns Uma promessa que resolve com os resultados dos usuários.
+ * @throws Se ocorrer um erro ao buscar os usuários.
+ */
+export async function getUsers(results = 1): Promise<User[]> {
+    try {
+        const response = await axios.get(`${BASE_URL}`, {
+            params: {
+                results,
+            },
+        });
+        if (response.status !== 200) {
+            throw new Error("Erro na solicitação à API");
         }
-    });
-
-    return response.data;
-
+        usersCache = response.data.results;
+        
+        return usersCache;
+    } catch (error) {
+        throw new Error("Erro ao buscar usuários: " + (error as Error).message);
+    }
 }
 
-export const api = {
-    getUsers,
-};
-
+/**
+ * Obtém um usuário pelo username a partir do cache.
+ * @param username - O username do usuário a ser buscado.
+ * @returns Uma promessa que resolve com o usuário encontrado.
+ * @throws Se o usuário não for encontrado no cache ou ocorrer um erro.
+ */
+export async function getUserByUserName(username: string): Promise<User> {
+    try {
+        const user = usersCache.find((u) => u.login.username === username);
+        if (!user) {
+            throw new Error("Usuário não encontrado no cache");
+        }
+        return user;
+    } catch (error) {
+        throw new Error("Erro ao buscar usuário por username: " + (error as Error).message);
+    }
+}
