d299506ef68d1e3e91fb2a67af8f84379091a906
Evandrosc <evandromdsc@gmail.com>
2023-09-09 23:26:50 -0300

    feat: react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom'
+import UserProfile from './components/UserProfile/index.tsx'
+import PageError from './components/PageError/index.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <PageError />,
+  },
+  {
+    path: '/user/:username',
+    element: <UserProfile />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
